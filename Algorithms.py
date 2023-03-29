bf95f3774c7103ee9dca7cdc49f903f35a31c22d
Jakub Krupiński <jakub.krupinski20.03@gmail.com>
2023-03-29 05:15:48 +0200

    A* with time and transfer criterions added

--- Algorithms.py
+++ Algorithms.py
@@ -1,7 +1,7 @@
 from graph import *
 import numpy as np
 import datetime
-import heapq, sys
+import heapq, sys, math
 from typing import Optional
 
 class Algorithms:
@@ -38,6 +38,27 @@ class Algorithms:
         self.cost[self.start_node.stop_name] = 0
         self.arrivals[self.start_node.stop_name] = time
         
+        
+    def retrieve_solution(self, my_focus: str) -> None:
+        while my_focus is not None:
+            self.arr_times.append(datetime.datetime.min + self.arrivals[my_focus])
+            self.dep_times.append(datetime.datetime.min + self.departures[my_focus])
+            self.used_lines.append(self.lines[my_focus])
+            self.path.append(my_focus)
+            my_focus = self.previous_nodes[my_focus]
+        self.arr_times.reverse()
+        self.dep_times.reverse()
+        self.used_lines.reverse()
+        self.path.reverse()            
+    
+    
+    def print_solution(self) -> None:
+        print(self.path[0])
+        for i in range(1, len(self.path)):
+            print(f">   {self.used_lines[i]} :   {self.dep_times[i].strftime('%H:%M:%S')}  -   {self.arr_times[i].strftime('%H:%M:%S')}")
+            print(self.path[i])
+            
+              
     def execute_dijkstra(self) -> None:
         while self.unsettled_nodes:
             priority, my_focus, current_time = heapq.heappop(self.unsettled_nodes)
@@ -50,6 +71,7 @@ class Algorithms:
                 else:
                     self.update_unsettled_dijkstra(my_focus, current_time) 
         self.retrieve_solution(my_focus)
+
 
     def update_unsettled_dijkstra(self, my_focus: str, current_time: datetime.timedelta) -> None:
         for edge in self.graph.edges[my_focus]:
@@ -67,25 +89,88 @@ class Algorithms:
                         self.arrivals[end_name] = edge.arrival_time
                         self.departures[end_name] = edge.departure_time
                         self.lines[end_name] = edge.line_name
-                        self.cost[end_name] = new_cost  #   new priority
                         self.previous_nodes[end_name] = my_focus
+                        self.cost[end_name] = new_cost  #   new priority
                         heapq.heappush(self.unsettled_nodes, (new_cost, edge.end_node.stop_name, edge.arrival_time))
                         
-    def retrieve_solution(self, my_focus: str) -> None:
-        while my_focus is not None:
-            self.arr_times.append(datetime.datetime.min + self.arrivals[my_focus])
-            self.dep_times.append(datetime.datetime.min + self.departures[my_focus])
-            self.used_lines.append(self.lines[my_focus])
-            self.path.append(my_focus)
-            my_focus = self.previous_nodes[my_focus]
-        self.arr_times.reverse()
-        self.dep_times.reverse()
-        self.used_lines.reverse()
-        self.path.reverse()            
+                        
+    def manhattan_dist(self, start: str, end: str) -> float:
+        return float(abs(self.graph.nodes[start].latitude - self.graph.nodes[end].latitude) 
+                     + abs(self.graph.nodes[start].longitude - self.graph.nodes[end].longitude))
+
+
+    def euclidean_dist(self, start: str, end: str) -> float:
+        return float(math.sqrt(pow(self.graph.nodes[start].latitude - self.graph.nodes[end].latitude, 2) 
+                         + pow(self.graph.nodes[start].longitude - self.graph.nodes[end].longitude, 2)))
+
+
+    def cost_of_line_transfer(self, prev: Edge, next: Edge) -> float:
+        if prev is not None and next is not None and prev.line_name != next.line_name:
+            return 1000
+        return 0
     
-    def print_solution(self) -> None:
-        print(self.path[0])
-        for i in range(1, len(self.path)):
-            print(f">   {self.used_lines[i]} :   {self.dep_times[i].strftime('%H:%M:%S')}  -   {self.arr_times[i].strftime('%H:%M:%S')}")
-            print(self.path[i])
+    
+    #   'criterion' string decides which crierion will be used in the a* algorithm,
+    #   which helps me reduce excess code, since the initial setup is the same
+    def execute_astar(self, criterion: str) -> None:
+        while self.unsettled_nodes:
+            priority, my_focus, current_time = heapq.heappop(self.unsettled_nodes)
+            self.settled_nodes.append(my_focus)
+            if my_focus == self.end_node.stop_name:
+                break
+            else:
+                if my_focus == "Zorawina - Niepodleglosci (Mostek)":
+                    continue
+                else:
+                    if criterion == 'time':
+                        self.update_unsettled_astar_time(priority, my_focus, current_time) 
+                    elif criterion == 'transfers':
+                        self.update_unsettled_astar_transfers(priority, my_focus, current_time) 
+                    else:
+                        print("Wrong criterion!")
+        self.retrieve_solution(my_focus)
+        
+    
+    def update_unsettled_astar_time(self, priority: any, my_focus: str, current_time: datetime.timedelta) -> None:
+        for edge in self.graph.edges[my_focus]:
+            total_seconds = edge.departure_time.total_seconds()
+            cmp_tool = (datetime.datetime.min + datetime.timedelta(seconds=total_seconds) - datetime.datetime.min)
             
+            if (self.lines[my_focus] == 0 and current_time == cmp_tool) \
+                or (edge.line_name != self.lines[my_focus] and current_time < cmp_tool) \
+                or (edge.line_name == self.lines[my_focus] and current_time == cmp_tool):
+                    new_cost = (edge.arrival_time - self.time).seconds / 60
+                    end_name = edge.end_node.stop_name
+                    if edge.end_node not in self.settled_nodes and new_cost < self.cost[end_name]:
+                        self.arrivals[end_name] = edge.arrival_time
+                        self.departures[end_name] = edge.departure_time
+                        self.lines[end_name] = edge.line_name
+                        self.previous_nodes[end_name] = my_focus
+                        self.cost[end_name] = new_cost  
+                        
+                        priority = new_cost + self.manhattan_dist(my_focus, end_name)
+                        heapq.heappush(self.unsettled_nodes, (priority, edge.end_node.stop_name, edge.arrival_time))
+                        
+                            
+    def update_unsettled_astar_transfers(self, priority: any, my_focus: str, current_time: datetime.timedelta) -> None:
+            for edge in self.graph.edges[my_focus]:
+                total_seconds = edge.departure_time.total_seconds()
+                cmp_tool = (datetime.datetime.min + datetime.timedelta(seconds=total_seconds) - datetime.datetime.min)
+                
+                if (self.lines[my_focus] == 0 and current_time == cmp_tool) \
+                    or (edge.line_name != self.lines[my_focus] and current_time < cmp_tool) \
+                    or (edge.line_name == self.lines[my_focus] and current_time == cmp_tool):
+                        new_cost = (edge.arrival_time - self.time).seconds / 60
+                        if edge.line_name != self.lines[my_focus]:
+                            new_cost += 25
+                        end_name = edge.end_node.stop_name
+                        if edge.end_node not in self.settled_nodes and new_cost < self.cost[end_name]:
+                            self.arrivals[end_name] = edge.arrival_time
+                            self.departures[end_name] = edge.departure_time
+                            self.lines[end_name] = edge.line_name
+                            self.previous_nodes[end_name] = my_focus
+                            self.cost[end_name] = new_cost  
+                            
+                            priority = new_cost + self.manhattan_dist(my_focus, end_name)
+                            heapq.heappush(self.unsettled_nodes, (priority, edge.end_node.stop_name, edge.arrival_time))
+    
--- main.py
+++ main.py
@@ -6,6 +6,27 @@ import random
 import numpy as np
 
 
+def main() -> None:
+    graph = DataReader.load_data()
+    starting_nodes, ending_nodes, starting_times = get_samples(graph)
+    #   dijkstra_sample100(graph, starting_nodes, ending_nodes, starting_times)
+    #   dijkstra_single(graph, "Psie Pole", "FAT", timedelta(hours=14, minutes=33))
+    #   astar_time_single(graph, "Psie Pole", "FAT", timedelta(hours=14, minutes=33))
+    #   astar_time_sample100(graph, starting_nodes, ending_nodes, starting_times)
+    astar_transfers_single(graph, "Psie Pole", "FAT", timedelta(hours=14, minutes=33))
+    #   astar_transfers_sample100(graph, starting_nodes, ending_nodes, starting_times)
+
+
+def calculate_average_runtimes(runtimes: list[float]) -> float:
+    runtimes.sort()
+    n_trim = int(0.05 * len(runtimes))
+    start_index = n_trim
+    end_index = len(runtimes) - n_trim
+
+    trimmed_runtimes = runtimes[start_index:end_index]
+    return np.mean(trimmed_runtimes)
+
+
 def get_samples(graph: Graph) -> tuple[list[str], list[str], list[timedelta]]:
     starting_nodes = random.sample(list(graph.nodes.keys()), 100)
     ending_nodes = random.sample(list(graph.nodes.keys()), 100)
@@ -18,17 +39,10 @@ def get_samples(graph: Graph) -> tuple[list[str], list[str], list[timedelta]]:
         while starting_nodes[i] == ending_nodes[i]:
             ending_nodes[i] = random.choice(list(graph.nodes.keys()))
     return (starting_nodes, ending_nodes, starting_times)
-
-
-def main() -> None:
-    graph = DataReader.load_data()
-    starting_nodes, ending_nodes, starting_times = get_samples(graph)
-    #   dijkstra_sample100(graph, starting_nodes, ending_nodes, starting_times)
-    dijkstra_single(graph, "KRZYKI", "FAT", timedelta(hours=14, minutes=33))
     
     
 def dijkstra_single(graph: Graph, beginning: str, destination: str, time: timedelta) -> None:
-    print("\n\n- - - - - - - - -DIJKSTRA- - - - - - - - - \n\n")
+    print("\n\n- - - - - - - - - -DIJKSTRA- - - - - - - - - -\n\n")
     print(f"Starting point: {beginning}")
     print(f"Destination: {destination}")
     print(f"Starting time: {time}")
@@ -44,7 +58,7 @@ def dijkstra_single(graph: Graph, beginning: str, destination: str, time: timede
 
 def dijkstra_sample100(graph: Graph, starting_nodes: list[str], ending_nodes: list[str], starting_times: list[timedelta]) -> None:
     dijkstra_runtimes = []
-    print("\n\n- - - - - - - - -DIJKSTRA- - - - - - - - - \n\n")
+    print("\n\n- - - - - - - - - -DIJKSTRA- - - - - - - - - -\n\n")
     for i in range(len(starting_nodes)):
         beginning = starting_nodes[i]
         destination = ending_nodes[i]
@@ -66,16 +80,88 @@ def dijkstra_sample100(graph: Graph, starting_nodes: list[str], ending_nodes: li
         print("")
     avg = calculate_average_runtimes(dijkstra_runtimes)
     print(f"Average runtime for finding a path with the dijkstra algorithm, from a random start, to a random destination, on a random time: {avg}")
+    
+    
+def astar_time_single(graph: Graph, beginning: str, destination: str, time: timedelta) -> None:
+    print("\n\n- - - - - - - - - -A* : TIME- - - - - - - - - -\n\n")
+    print(f"Starting point: {beginning}")
+    print(f"Destination: {destination}")
+    print(f"Starting time: {time}")
 
+    astar_time = Algorithms(graph, time, beginning, destination)
+    start_time = datetime.now()
+    astar_time.execute_astar('time')
+    end_time = datetime.now()
+    runtime = (end_time - start_time).total_seconds()
+    print(f"A* (time criterion) runtime in seconds: {runtime:.2f}")
+    astar_time.print_solution()
+    
+    
+def astar_time_sample100(graph: Graph, starting_nodes: list[str], ending_nodes: list[str], starting_times: list[timedelta]) -> None:
+    astar_t_runtimes = []
+    print("\n\n- - - - - - - - - -A* : TIME- - - - - - - - - -\n\n")
+    for i in range(len(starting_nodes)):
+        beginning = starting_nodes[i]
+        destination = ending_nodes[i]
+        time = starting_times[i]
+        tmp_graph = graph
 
-def calculate_average_runtimes(runtimes: list[float]) -> float:
-    runtimes.sort()
-    n_trim = int(0.05 * len(runtimes))
-    start_index = n_trim
-    end_index = len(runtimes) - n_trim
+        print(f"Starting point: {beginning}")
+        print(f"Destination: {destination}")
+        print(f"Starting time: {time}")
 
-    trimmed_runtimes = runtimes[start_index:end_index]
-    return np.mean(trimmed_runtimes)
+        astar_time = Algorithms(tmp_graph, time, beginning, destination)
+        start_time = datetime.now()
+        astar_time.execute_astar('time')
+        end_time = datetime.now()
+        runtime = (end_time - start_time).total_seconds()
+        print(f"A* (time criterion) runtime in seconds: {runtime:.2f}")
+        astar_t_runtimes.append(runtime)
+        astar_time.print_solution()
+        print("")
+    avg = calculate_average_runtimes(astar_t_runtimes)
+    print(f"Average runtime for finding a path with the A* algorithm with a time criterion, from a random start, to a random destination, on a random time: {avg}")
+    
+    
+def astar_transfers_single(graph: Graph, beginning: str, destination: str, time: timedelta) -> None:
+    print("\n\n- - - - - - - - - -A* : TIME- - - - - - - - - -\n\n")
+    print(f"Starting point: {beginning}")
+    print(f"Destination: {destination}")
+    print(f"Starting time: {time}")
+
+    astar_time = Algorithms(graph, time, beginning, destination)
+    start_time = datetime.now()
+    astar_time.execute_astar('transfers')
+    end_time = datetime.now()
+    runtime = (end_time - start_time).total_seconds()
+    print(f"A* (transfer criterion) runtime in seconds: {runtime:.2f}")
+    astar_time.print_solution()
+    
+    
+def astar_transfers_sample100(graph: Graph, starting_nodes: list[str], ending_nodes: list[str], starting_times: list[timedelta]) -> None:
+    astar_t_runtimes = []
+    print("\n\n- - - - - - - - - -A* : TIME- - - - - - - - - -\n\n")
+    for i in range(len(starting_nodes)):
+        beginning = starting_nodes[i]
+        destination = ending_nodes[i]
+        time = starting_times[i]
+        tmp_graph = graph
+
+        print(f"Starting point: {beginning}")
+        print(f"Destination: {destination}")
+        print(f"Starting time: {time}")
+
+        astar_time = Algorithms(tmp_graph, time, beginning, destination)
+        start_time = datetime.now()
+        astar_time.execute_astar('transfers')
+        end_time = datetime.now()
+        runtime = (end_time - start_time).total_seconds()
+        print(f"A* (transfer criterion) runtime in seconds: {runtime:.2f}")
+        astar_t_runtimes.append(runtime)
+        astar_time.print_solution()
+        print("")
+    avg = calculate_average_runtimes(astar_t_runtimes)
+    print(f"Average runtime for finding a path with the A* algorithm with a time criterion, from a random start, to a random destination, on a random time: {avg}")
 
 
 if __name__ == '__main__':
